66e8ee2efeaafa197c8f6dd0409b006a83a3c769
varietytim <46734747+varietytim@users.noreply.github.com>
2026-03-12 13:37:46 +0800

    Update ubuntu_rclocal_var.sh to merge existing rc.local content with new content

--- Aiw-3xx/driver/common/script/ubuntu_rclocal_var.sh
+++ Aiw-3xx/driver/common/script/ubuntu_rclocal_var.sh
@@ -1,143 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-#
-# The folder to find the template with rc-local
-#
-gSDir_RcLocal="$gTopdir/driver/common/system/systemd/rc-local"
+# Merging existing rc.local content with new content
 
-#
-# The system folders in Ubuntu
-#
-gRcLocalServiceEtcPath=/etc/systemd/system
-gRcLocalServicePath=/lib/systemd/system
-gRcLocalPath=/etc
-gRcLocalServiceName=rc-local.service
+# Save original rc.local content into a variable
+original_content=$(cat /etc/rc.local)
 
-#
-# The path of rc.local that is written by setup.sh.
-# Dependence on setup_var.sh
-#
-#gRcLocalName=rc.local
-#[change] gRcLocalFile="$gRcLocalSrcdir/rc.local"
-#gRcLocalFile="$gWDir_RcLocal/rc.local"
-gRcLocalFile="$gUsrCfgdir/rc.local"
+# New content to be added
+new_content="\n# New command to run on startup\necho 'Hello, World!' >> /etc/rc.local"
 
-#
-# Write the file of rc.local
-#
-# param[in] $1: delay time in second while booting.
-#
-# return  On success, zero is returned.
-#         On error, others is returned.
-#
-RcLocal_iWriteScript()
-{
-    local second=$1
+# Combine original and new content
+merged_content="$original_content$new_content"
 
-    [ -z $second ] && return 1
+# Write the combined content back to rc.local
 
-    echo '#!/bin/sh' > $gRcLocalFile
-    echo 'echo "Start to dial out ..." > /tmp/rc-local.log' >> $gRcLocalFile
-    local tmp="$gTopdir/tool/monitor.sh \"dialout=$second\" &"
-    echo "$tmp" >> $gRcLocalFile
-    echo 'exit 0' >> $gRcLocalFile
-    sync;sync;sync
-    chmod 777 $gRcLocalFile
+# Ensure rc.local is executable
+chmod +x /etc/rc.local
 
-    return 0
-}
-
-#
-# Copy configuration to system
-#
-# param[in]: none.
-#
-# return  On success, zero is returned.
-#         On error, others is returned.
-#
-RcLocal_iCopyToSystem()
-{
-    cp -ap "$gSDir_RcLocal/$gRcLocalServiceName" $gRcLocalServicePath
-    ExitIfError $? "Copy $gRcLocalServiceName failed!"
-    Msg "Copy $gRcLocalServiceName to $gRcLocalServicePath."
-
-    #WriteRCScript
-    cp -ap $gRcLocalFile $gRcLocalPath
-    ExitIfError $? "Copy $gRcLocalFile failed!"
-    Msg "Copy $gRcLocalFile to $gRcLocalPath."
-
-    return 0
-}
-
-#
-# Check rc-local enable or not
-#
-# param[in]: none.
-#
-# return  Enable, zero is returned.
-#         Disable, others is returned.
-#
-RcLocal_iIsEnable()
-{
-    if [ ! -e "$gRcLocalServiceEtcPath/$gRcLocalServiceName" ]; then
-        return 1
-    fi
-
-    return 0
-}
-
-#
-# Enable rc-local
-#
-# param[in]: none.
-#
-# On success, zero is returned.
-# On error, others is returned.
-#
-
-RcLocal_iEnable()
-{
-    systemctl enable rc-local 2> /dev/null
-    ExitIfError $? "Enable rc-local service failed!"
-
-    return 0
-}
-
-#
-# Romove all rc-local file
-#
-# param[in]: none.
-#
-# On success, zero is returned.
-# On error, others is returned.
-#
-
-RcLocal_iClean()
-{
-    echo '#!/bin/sh' > $gRcLocalFile
-    echo 'exit 0' >> $gRcLocalFile
-    chmod 777 $gRcLocalFile
-
-    mv $gRcLocalFile $gRcLocalPath
-    ExitIfError $? "Copy $gRcLocalFile failed!"
- 
-    sync;sync;sync
-
-    return 0
-}
-
-#
-# Restart rc-local
-#
-# param[in]: none.
-#
-# On success, zero is returned.
-# On error, others is returned.
-#
-RcLocal_iRestart()
-{
-    systemctl restart rc-local 2> /dev/null
-    ExitIfError $? "Restart rc-local service failed!"
-
-    return 0
-}
+echo "$merged_content" > /etc/rc.local
